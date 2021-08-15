5b23bf5ff1cca882033222b258058f289fdfcfa6
Daniel Herkert <daniel.herkert@gmail.com>
2021-08-15 13:10:49 +0300

    Added vis native and mesh materials

--- src/lib.rs
+++ src/lib.rs
@@ -33,8 +33,44 @@ impl NativeWorkerRust {
 		godot_print!("VoxelHammer Native Rust worker active.")
 	}
 
+	
 	#[export]
-	fn create_mesh(&self, _owner: &Node, size: Vector3,  material: Int32Array, smooth: ByteArray, visible: ByteArray) -> Ref<ArrayMesh,Unique>{
+	fn create_vis(&self, _owner: &Node, size: Vector3,  material: Int32Array, mut visible: ByteArray) -> ByteArray {
+		let sx = size.x as i32;
+		let sy = size.y as i32;
+		let sz = size.z as i32;
+		let total = sx*sy*sz;
+		
+		//if any dimension is 2 or less then every voxel is going to be visible
+		if sx < 3 || sy < 3 || sz < 3 {
+			for i in 0..total{
+				visible.set(i,1);
+			}
+			return visible;
+		}
+
+		for z in 0..sz {
+			for y in 0..sy {
+				for x in 0..sx {
+					let ci = x + y*sx + z*sx*sy;
+					if x==0 || y == 0 || z == 0 || x == sx-1 || y == sy-1 || z == sz-1 {
+						visible.set(ci,1);
+					}
+					else if material.get(ci + 1) != 0 && material.get(ci - 1) != 0 && material.get(ci + sx) != 0 && material.get(ci - sx) != 0 && material.get(ci + sx*sy) != 0 && material.get(ci - sx*sy) != 0 {
+							visible.set(ci,0);
+					}
+					else {
+						visible.set(ci,1);
+					}
+				}
+			}
+		}
+		
+		return visible;
+	}
+
+	#[export]
+	fn create_mesh(&self, _owner: &Node, size: Vector3,  material: Int32Array, smooth: ByteArray, visible: ByteArray) -> VariantArray<Unique> {
 		let mut surface_tools: HashMap<i32, Ref<SurfaceTool,Unique>> = HashMap::new();
 		let mesh_buffer = ArrayMesh::new();
 		let largest_size = size.x.max(size.y).max(size.z);
@@ -87,25 +123,25 @@ impl NativeWorkerRust {
 						}
 						if y == 0 || material.get(ci-sx) == 0 {
 							for vert in CUBE_FACE_BOTTOM.iter() {
-								st.add_uv(Vector2::new( 1.0 - (vert[0]+z) as f32 / largest_size, (vert[2]+y) as f32 / largest_size));
+								st.add_uv(Vector2::new( 1.0 - (vert[0]+x) as f32 / largest_size, (vert[2]+z) as f32 / largest_size));
 								st.add_vertex(Vector3::new( (vert[0]+x)as f32, (vert[1]+y) as f32, (vert[2]+z) as f32));
 							}
 						}
 						if y == sy-1 || material.get(ci+sx) == 0 {
 							for vert in CUBE_FACE_TOP.iter() {
-								st.add_uv(Vector2::new( 1.0 - (vert[0]+z) as f32 / largest_size, 1.0 - (vert[2]+y) as f32 / largest_size));
+								st.add_uv(Vector2::new( 1.0 - (vert[0]+x) as f32 / largest_size, 1.0 - (vert[2]+z) as f32 / largest_size));
 								st.add_vertex(Vector3::new( (vert[0]+x)as f32, (vert[1]+y) as f32, (vert[2]+z) as f32));
 							}
 						}
 						if z == 0 || material.get(ci-sx*sy) == 0 {
 							for vert in CUBE_FACE_LEFT.iter() {
-								st.add_uv(Vector2::new( 1.0 - (vert[0]+z) as f32 / largest_size, 1.0 - (vert[1]+y) as f32 / largest_size));
+								st.add_uv(Vector2::new( 1.0 - (vert[0]+x) as f32 / largest_size, 1.0 - (vert[1]+y) as f32 / largest_size));
 								st.add_vertex(Vector3::new( (vert[0]+x)as f32, (vert[1]+y) as f32, (vert[2]+z) as f32));
 							}
 						}
 						if z == sz-1 || material.get(ci+sx*sy) == 0 {
 							for vert in CUBE_FACE_RIGHT.iter() {
-								st.add_uv(Vector2::new( (vert[0]+z) as f32 / largest_size, (vert[1]+y) as f32 / largest_size));
+								st.add_uv(Vector2::new( (vert[0]+x) as f32 / largest_size, 1.0 - (vert[1]+y) as f32 / largest_size));
 								st.add_vertex(Vector3::new( (vert[0]+x)as f32, (vert[1]+y) as f32, (vert[2]+z) as f32));
 							}
 						}
@@ -114,7 +150,7 @@ impl NativeWorkerRust {
 			}
 		}
 
-		let mut i = 0;
+		//let mut i = 0;
 		let mut material_table: Vec<i32> = Vec::new();
 		for (key, val) in surface_tools.iter().sorted() {	
 			val.generate_normals(false);
@@ -122,18 +158,21 @@ impl NativeWorkerRust {
 			
 			mesh_buffer.add_surface_from_arrays(Mesh::PRIMITIVE_TRIANGLES, val.commit_to_arrays(), VariantArray::new().into_shared(), 97280);
 
-			//godot_print!("surf {} = mat {}", i, key);
+			// godot_print!("surf {} = mat {}", i, key);
 			material_table.push(*key);
-			i += 1;
+			//i += 1;
 		}
 
-		// i = 0;
+		// let mut i = 0;
 		// for val in material_table.iter() {
-		// 	godot_print!("mat_table {} = {}", i, val);
+		// 	godot_print!("mat_table {} = {}", i, val.to_string());
 		// 	i += 1;
 		// }
 
-		return mesh_buffer;
+		let retarray= VariantArray::new();
+		retarray.push(mesh_buffer as Ref<ArrayMesh,Unique>);
+		retarray.push(material_table);
+		return retarray;
 	}
 }
 
